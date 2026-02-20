de3ff1bf0cad9bbf6a764c67a4fefeab43711832
Kizand <tatyana-kizand@yandex.ru>
2026-02-20 12:59:20 +0300

    Add predators

---
+++ src/main/java/com/javarush/island/organisms/predator/Bear.java
@@ -0,0 +1,10 @@
+package com.javarush.island.organisms.predator;
+
+import com.javarush.island.config.OrganismConfig;
+
+public class Bear extends Predator {
+
+    public Bear(String name, OrganismConfig config) {
+        super(name, config);
+    }
+}
---
+++ src/main/java/com/javarush/island/organisms/predator/Wolf.java
@@ -0,0 +1,10 @@
+package com.javarush.island.organisms.predator;
+
+import com.javarush.island.config.OrganismConfig;
+
+public class Wolf extends Predator {
+
+    public Wolf(String name, OrganismConfig config) {
+        super(name, config);
+    }
+}
