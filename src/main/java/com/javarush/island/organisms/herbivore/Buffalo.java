ec73577943d997ae2156f5ec99136f125c4cec83
Kizand <tatyana-kizand@yandex.ru>
2026-02-20 12:55:40 +0300

    Add herbivores

---
+++ src/main/java/com/javarush/island/organisms/herbivore/Buffalo.java
@@ -0,0 +1,10 @@
+package com.javarush.island.organisms.herbivore;
+
+import com.javarush.island.config.OrganismConfig;
+
+public class Buffalo extends Herbivore {
+
+    public Buffalo(String name, OrganismConfig config) {
+        super(name, config);
+    }
+}
---
+++ src/main/java/com/javarush/island/organisms/herbivore/Duck.java
@@ -0,0 +1,10 @@
+package com.javarush.island.organisms.herbivore;
+
+import com.javarush.island.config.OrganismConfig;
+
+public class Duck extends Herbivore {
+
+    public Duck(String name, OrganismConfig config) {
+        super(name, config);
+    }
+}
